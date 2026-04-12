24dcdf4f099a0caf5685ae0a2c4f0e87c86596a1
CrashingStatic <crashingstatic@github.com>
2026-04-12 00:51:02 -0400

    Removed extra entry for debian apt in firewall

--- .devcontainer/init-firewall.sh
+++ .devcontainer/init-firewall.sh
@@ -73,8 +73,7 @@ for domain in \
     "marketplace.visualstudio.com" \
     "vscode.blob.core.windows.net" \
     "update.code.visualstudio.com" \
-    "deb.debian.org" \
-    "security.debian.org"; do
+    "deb.debian.org"; do
     echo "Resolving $domain..."
     ips=$(dig +noall +answer A "$domain" | awk '$4 == "A" {print $5}')
     if [ -z "$ips" ]; then
